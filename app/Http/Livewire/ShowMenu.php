6ca010fabc4d5fe6a919553ac0254365e901f017
hardhikapro <propitadewa@gmail.com>
2022-05-29 15:49:54 +0700

    Show Menu done

---
+++ app/Http/Livewire/ShowMenu.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ShowMenu extends Component
+{
+    public function render()
+    {
+        return view('livewire.show-menu');
+    }
+}
