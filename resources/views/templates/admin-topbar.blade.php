0e049f918286b4e79f09372e8a1592c45d04aaea
M. Fiqri Ananda <muh.fikri.ananda@gmail.com>
2022-06-08 18:59:20 +0700

    Add auth()->user()->name in top bar

--- resources/views/templates/admin-topbar.blade.php
+++ resources/views/templates/admin-topbar.blade.php
@@ -49,7 +49,7 @@
         <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
                 aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Naufal Rafiawan Basara</span>
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{auth()->user()->name}}</span>
                 <img class="img-profile rounded-circle" src="{{ asset('img/admin/undraw_profile.svg') }}">
             </a>
             <!-- Dropdown - User Information -->
